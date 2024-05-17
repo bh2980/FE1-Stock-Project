7723586127ff246ac4e9da664135d5136e09cf73
bh2980 <bh2980@naver.com>
2024-05-17 21:09:10 +0900

    refactor: ♻️ Axis와 Bar 분리

--- src/components/molecules/BarChart/BarChart.tsx
+++ src/components/molecules/BarChart/BarChart.tsx
@@ -1,7 +1,11 @@
-import { max, scaleBand, scaleLinear } from "d3";
-import React from "react";
+import { type ScaleBand, type ScaleLinear, max, scaleBand, scaleLinear } from "d3";
+import React, { PropsWithChildren } from "react";
 
-type BarChartDataType = { year: number; value: number | null };
+import { PolymorphicPropsType, type PolymorphicPropsWithInnerRefType } from "@customTypes/polymorphicType";
+
+import { isEven } from "@utils/isEven";
+
+type BarChartDataType = { label: number; value: number | null };
 
 type BarChartProps = {
   width: number;
@@ -9,15 +13,51 @@ type BarChartProps = {
   data: BarChartDataType[];
 };
 
+type BandAxisProps = PolymorphicPropsWithInnerRefType<"g"> & {
+  xScale: ScaleBand<string>;
+  outerTickLength?: number;
+  innerTickLength?: number;
+};
+
+const BandAxis = ({ xScale, outerTickLength = 6, innerTickLength = 6, ...props }: BandAxisProps) => {
+  const [startPoint, endPoint] = xScale.range();
+
+  const tickStartPoint = (endPoint - startPoint) / 2 - xScale.step() * (isEven(xScale.domain().length) ? xScale.domain().length / 2 - 0.5 : Math.floor(xScale.domain().length / 2));
+
+  return (
+    <g {...props}>
+      <path fill="none" d={`M${startPoint},${outerTickLength}V0H${endPoint}V${outerTickLength}`}></path>
+      {xScale.domain().map((label, i) => (
+        <g key={`tick-${i}`} transform={`translate(${tickStartPoint + xScale.step() * i}, 0)`}>
+          <line y2={innerTickLength} />
+          <text y="24" stroke="none">
+            {label}
+          </text>
+        </g>
+      ))}
+    </g>
+  );
+};
+
+type BarProps = PolymorphicPropsType<"rect"> &
+  PropsWithChildren & {
+    xScale: ScaleBand<string>;
+    yScale: ScaleLinear<number, number, never>;
+    data: BarChartDataType;
+    nullBarHeight?: number;
+  };
+
+const Bar = ({ xScale, yScale, data, nullBarHeight = 0, ...props }: BarProps) => {
+  return <rect width={xScale.bandwidth()} height={yScale(0) - yScale(data.value || nullBarHeight)} x={xScale(data.label.toString())} y={yScale(data.value || nullBarHeight)} {...props} />;
+};
+
 const BarChart = ({ width, height, data }: BarChartProps) => {
   const nullBarHeight = 50;
   const barRadius = 6;
   const margin = { left: 0, bottom: 32 };
-  const outerTickLength = 6;
-  const innerTickLength = 6;
 
   const xScale = scaleBand()
-    .domain(data.map((d) => d.year.toString()))
+    .domain(data.map((d) => d.label.toString()))
     .range([margin.left, width - margin.left])
     .padding(0.5);
 
@@ -26,42 +66,28 @@ const BarChart = ({ width, height, data }: BarChartProps) => {
     .nice()
     .range([height - margin.bottom, margin.bottom]);
 
-  const tickStartPoint = (xScale.range()[1] - xScale.range()[0]) / 2 - xScale.step() * (xScale.domain().length % 2 === 0 ? xScale.domain().length / 2 - 0.5 : Math.floor(xScale.domain().length / 2));
-
   return (
     <svg width={width} height={height}>
       {data.map((d, i) => (
         <g key={`bar-${i}`}>
-          <rect
-            width={xScale.bandwidth()}
-            height={yScale(0) - yScale(d.value || nullBarHeight)}
-            x={xScale(d.year.toString())}
-            y={yScale(d.value || nullBarHeight)}
-            rx={barRadius}
-            stroke="currentColor"
-            strokeDasharray={d.value === null ? "6, 4" : undefined}
-            className={d.value === null ? "fill-none" : "fill-secondary"}
-          >
-            <animate attributeName="height" from="0" to={yScale(0) - yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
-            <animate attributeName="y" from={yScale(0)} to={yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
-          </rect>
-          <text x={xScale(d.year.toString())! + xScale.bandwidth() / 2} y={yScale(d.value || nullBarHeight) - 8} textAnchor="middle" className="text-surface-on font-bold text-body2">
+          {d.value !== null ? (
+            <Bar xScale={xScale} yScale={yScale} data={d} nullBarHeight={50} rx="6" className="fill-secondary">
+              <animate attributeName="height" from="0" to={yScale(0) - yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
+              <animate attributeName="y" from={yScale(0)} to={yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
+            </Bar>
+          ) : (
+            <Bar xScale={xScale} yScale={yScale} data={{ ...d, value: 50 }} rx={barRadius} strokeDasharray="6,4" className="stroke-black fill-none">
+              <animate attributeName="height" from="0" to={yScale(0) - yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
+              <animate attributeName="y" from={yScale(0)} to={yScale(d.value || nullBarHeight)} dur="0.5s" fill="freeze" />
+            </Bar>
+          )}
+          <text x={xScale(d.label.toString())! + xScale.bandwidth() / 2} y={yScale(d.value || nullBarHeight) - 8} textAnchor="middle" className="text-surface-on font-bold text-body2">
             {d.value ? `${d.value}억` : "?"}
             <animate attributeName="y" from={yScale(0) - 8} to={yScale(d.value || nullBarHeight) - 8} dur="0.5s" fill="freeze" />
           </text>
         </g>
       ))}
-      <g transform={`translate(0, ${height - 32})`}>
-        <path stroke="none" fill="none" d={`M${xScale.range()[0]},${outerTickLength}V0H${xScale.range()[1]}V${outerTickLength}`}></path>
-        {xScale.domain().map((label, i) => (
-          <g key={`tick-${i}`} transform={`translate(${tickStartPoint + xScale.step() * i}, 0)`} textAnchor="middle">
-            <line stroke="none" y2={innerTickLength} />
-            <text className="text-body2 text-secondary-container" y="24">
-              {label}
-            </text>
-          </g>
-        ))}
-      </g>
+      <BandAxis xScale={xScale} textAnchor="middle" transform={`translate(0, ${height - margin.bottom})`} className="stroke-none text-body1 fill-surface-on-variant" />
     </svg>
   );
 };
