9b79676d1c8e8b9dd3a3e7a6010bae2621eef2ea
bh2980 <bh2980@naver.com>
2024-05-17 18:09:54 +0900

    feat: ✨ 축 컴포넌트 화

--- src/components/molecules/BarChart/BarChart.tsx
+++ src/components/molecules/BarChart/BarChart.tsx
@@ -1,4 +1,4 @@
-import * as d3 from "d3";
+import { max, scaleBand, scaleLinear } from "d3";
 import React from "react";
 
 type BarChartDataType = { year: number; value: number | null };
@@ -13,20 +13,20 @@ const BarChart = ({ width, height, data }: BarChartProps) => {
   const nullBarHeight = 50;
   const barRadius = 6;
   const margin = { left: 0, bottom: 32 };
+  const outerTickLength = 6;
+  const innerTickLength = 6;
 
-  const xScale = d3
-    .scaleBand()
+  const xScale = scaleBand()
     .domain(data.map((d) => d.year.toString()))
     .range([margin.left, width - margin.left])
     .padding(0.4);
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => (d.value ? d.value : 0))!])
+  const yScale = scaleLinear()
+    .domain([0, max(data, (d) => (d.value ? d.value : 0))!])
     .nice()
     .range([height - margin.bottom, margin.bottom]);
 
-  console.log(xScale);
+  const tickStartPoint = (xScale.range()[1] - xScale.range()[0]) / 2 - xScale.step() * (xScale.domain().length % 2 === 0 ? xScale.domain().length / 2 - 0.5 : Math.floor(xScale.domain().length / 2));
 
   return (
     <svg width={width} height={height}>
@@ -38,13 +38,26 @@ const BarChart = ({ width, height, data }: BarChartProps) => {
             x={xScale(d.year.toString())}
             y={yScale(d.value || nullBarHeight)}
             rx={barRadius}
-            className="fill-secondary"
+            stroke="currentColor"
+            strokeDasharray={d.value === null ? "6, 4" : undefined}
+            className={d.value === null ? "fill-none" : "fill-secondary"}
           />
           <text x={xScale(d.year.toString())! + xScale.bandwidth() / 2} y={yScale(d.value || nullBarHeight) - 8} textAnchor="middle" className="text-surface-on font-bold text-body2">
             {d.value ? `${d.value}억` : "?"}
           </text>
         </g>
       ))}
+      <g transform={`translate(0, ${height - 32})`}>
+        <path stroke="none" fill="none" d={`M${xScale.range()[0]},${outerTickLength}V0H${xScale.range()[1]}V${outerTickLength}`}></path>
+        {xScale.domain().map((label, i) => (
+          <g key={`tick-${i}`} transform={`translate(${tickStartPoint + xScale.step() * i}, 0)`} textAnchor="middle">
+            <line stroke="none" y2={innerTickLength} />
+            <text className="text-body2 text-secondary-container" y="24">
+              {label}
+            </text>
+          </g>
+        ))}
+      </g>
     </svg>
   );
 };
